0920058f94d04368ebd022bb16953384a9243f27
Vukasin <vule2002.vc@gmail.com>
2023-04-26 23:49:39 +0200

    Added the initial backend

---
+++ kvt-svt-projekat/src/main/java/com/ftn/kvtsvtprojekat/KvtSvtProjekatApplication.java
@@ -0,0 +1,13 @@
+package com.ftn.kvtsvtprojekat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KvtSvtProjekatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KvtSvtProjekatApplication.class, args);
+	}
+
+}
---
+++ kvt-svt-projekat/src/test/java/com/ftn/kvtsvtprojekat/KvtSvtProjekatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.kvtsvtprojekat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KvtSvtProjekatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
