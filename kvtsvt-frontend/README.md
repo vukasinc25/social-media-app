dbe9cf71c6cb44aba4589ec0f49d0708b714258a
Vukasin <vule2002.vc@gmail.com>
2023-05-20 23:23:09 +0200

    Changed the angular app name

--- kvtsvt-frontend/README.md
+++ kvtsvt-frontend/README.md
@@ -1,4 +1,4 @@
-# KvtsvtProject
+# KvtsvtFrontend
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.2.5.
 
--- kvtsvt-frontend/src/app/app.component.spec.ts
+++ kvtsvt-frontend/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'kvtsvtProject'`, () => {
+  it(`should have as title 'kvtsvt-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('kvtsvtProject');
+    expect(app.title).toEqual('kvtsvt-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('kvtsvtProject app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('kvtsvt-frontend app is running!');
   });
 });
